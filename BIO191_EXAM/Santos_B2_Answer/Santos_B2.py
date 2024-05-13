6342ffa59373401f179a0f2a29df61ca50392039
rameahsantos <167048142+rameahsantos@users.noreply.github.com>
2024-05-13 13:11:33 +0800

    Add files via upload

--- BIO191_EXAM/Santos_B2_Answer/Santos_B2.py
+++ BIO191_EXAM/Santos_B2_Answer/Santos_B2.py
@@ -1,13 +1,4 @@
-import pandas as pd
-df = pd.read_csv("Exam_Table_Graded.csv")
-average = df.groupby ("Stage") ["Cry1Ac,ppmDW"].mean()
-output = pd.DataFrame({"Stage": ["Vegetative"], "Cry1Ac,ppmDW" : average["Vegetative"]})
-
-average = df.groupby ("Stage") ["Cry1Ac,ppmDW"].mean()
-output = pd.DataFrame({"Stage": ["Reproductive"], "Cry1Ac,ppmDW" : average["Reproductive"]})
-
-
-output.to_csv("./Santos_B2.csv")
-             
-                               
-                               
+import pandas, os
+df = pandas.read_csv("Exam_Table_Graded.csv")
+mean = df.groupby("Stage")["Cry1Ac,ppmDW"].mean().reset_index()
+mean.to_csv("b2_output.csv", index=False)
